ca5bd9c8ea7618fab8f7f7a22b3cc88c26ed568c
Vitor Augusto de Mattos <vitoraugusto2010201078@gmail.com>
2023-11-23 13:17:00 -0300

    feature new styles and function

--- src/js/calculateSum.js
+++ src/js/calculateSum.js
@@ -1,13 +1,13 @@
 function calculateDivisibleSum(number) {
-    if (!number) throw new Error('Por favor, insira um número.');
-    if (typeof number !== 'number' || !Number.isInteger(number) || number <= 0) {
-        throw new Error('Por favor, insira um número inteiro positivo.');
+  if (!number) throw new Error("Por favor, insira um número.");
+  if (typeof number !== "number" || !Number.isInteger(number) || number <= 0) {
+    throw new Error("Por favor, insira um número inteiro positivo.");
+  }
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
     }
-    let sum = 0;
-    for (let i = 1; i < number; i++) {
-        if (i % 3 === 0 || i % 5 === 0) {
-            sum += i;
-        }
-    }
-    return sum;
+  }
+  return sum;
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,20 @@
 function calculateSum() {
-    const inputNumber = document.getElementById('inputNumber').value;
-    try {
-        const result = calculateDivisibleSum(parseInt(inputNumber, 10));
-        document.getElementById('result').innerText = `O somatório é: ${result}`;
-    } catch (error) {
-        document.getElementById('result').innerText = error.message;
-    }
+  const inputNumber = document.getElementById("inputNumber");
+  const resultElement = document.getElementById("result");
+  try {
+    const result = calculateDivisibleSum(parseInt(inputNumber.value, 10));
+    inputNumber.classList.remove("error-input");
+    inputNumber.classList.add("success-input");
+    resultElement.innerHTML = `O somatório é: <strong> ${result}</strong>`;
+  } catch (error) {
+    inputNumber.classList.add("error-input");
+    inputNumber.classList.remove("success-input");
+    resultElement.innerText = error.message;
+  }
 }
 
 function clearResult() {
-    document.getElementById('result').innerText = '';
+  const inputNumber = document.getElementById("inputNumber");
+  inputNumber.classList.remove("error-input", "success-input");
+  document.getElementById("result").innerText = "";
 }
