6e52f5279c4ac902586c0e37956349acaf307ab5
Vitor Augusto de Mattos <vitoraugusto2010201078@gmail.com>
2023-11-23 08:54:30 -0300

    feature/created function calculateSum and Html (first commit)

---
+++ src/js/calculateSum.js
@@ -0,0 +1,12 @@
+function calculateDivisibleSum(number) {
+    if (typeof number !== 'number' || !Number.isInteger(number) || number <= 0) {
+        throw new Error('Por favor, insira um número inteiro positivo.');
+    }
+    let sum = 0;
+    for (let i = 1; i < number; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+function calculateSum() {
+    const inputNumber = document.getElementById('inputNumber').value;
+    try {
+        const result = calculateDivisibleSum(parseInt(inputNumber, 10));
+        document.getElementById('result').innerText = `O somatório é: ${result}`;
+    } catch (error) {
+        document.getElementById('result').innerText = error.message;
+    }
+}
