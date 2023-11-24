75933f16611fd35dc9c3b4300a589c6f41af5d2a
Vitor Augusto de Mattos <vitoraugusto2010201078@gmail.com>
2023-11-24 08:55:43 -0300

    fixed calculateSum and calculateDivisibleSum just integer

--- src/js/calculateSum.js
+++ src/js/calculateSum.js
@@ -3,6 +3,10 @@ function calculateDivisibleSum(number) {
   if (typeof number !== "number" || !Number.isInteger(number) || number <= 0) {
     throw new Error("Por favor, insira um número inteiro positivo.");
   }
+  if (number > 99999) {
+    throw new Error("Por favor, insira um número com no máximo 5 dígitos.");
+  }
+  //Por questão de performance, o valor máximo de number é 99999.
   let sum = 0;
   for (let i = 1; i < number; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 function calculateSum() {
   const inputNumber = document.getElementById("inputNumber");
   const resultElement = document.getElementById("result");
+  const inputValue = inputNumber.value.trim();
+
+  if (!Number.isInteger(parseFloat(inputValue))) {
+    inputNumber.classList.add("error-input");
+    inputNumber.classList.remove("success-input");
+    resultElement.innerText = "Por favor, insira um número inteiro.";
+    return;
+  }
   try {
     const result = calculateDivisibleSum(parseInt(inputNumber.value, 10));
     inputNumber.classList.remove("error-input");
